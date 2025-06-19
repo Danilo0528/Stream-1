10407ae62cb6b68248233632095e638eeb6a4668
Danilo0528 <dpino131@gmail.com>
2025-06-19 13:29:28 -0500

    Reajuste de servidor

--- stream.sh
+++ stream.sh
@@ -39,14 +39,9 @@ service nginx start
 
 #ffmpeg -stream_loop -1 \
 #    -re -i "http://localhost:$SERVER_PORT/video.mp4" \
-#    -c:v libx264 -preset ultrafast -maxrate 1200k -bufsize 2400k -g 50 \
+#    -c:v libx264 -preset ultrafast -maxrate 1600k -bufsize 3400k -g 50 \
 #    -c:a aac -b:a 128k -ar 44100 \
 #    -f flv "rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY"
 
-ffmpeg -stream_loop -1 \
-    -re -i "http://localhost:$SERVER_PORT/video.mp4" \
-    -vf "scale=854:480" \
-    -c:v libx264 -preset ultrafast -maxrate 1600k -bufsize 3200k -g 50 \
-    -c:a aac -b:a 128k -ar 44100 \
-    -f flv "rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY"
+
     
