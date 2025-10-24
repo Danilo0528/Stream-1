3eaf03ea4de168d7e4bf1facb7e151a59aeacd09
Danilo0528 <dpino131@gmail.com>
2025-10-23 20:26:57 -0500

    update

--- stream.sh
+++ stream.sh
@@ -1,49 +1,17 @@
 #!/bin/bash
 
 # Configuración de variables
-VIDEO_FILE="${VIDEO_FILE:-rickroll.mp4}"
 YOUTUBE_KEY="${YOUTUBE_KEY:-your-youtube-code}"
-SERVER_PORT="${PORT:-8080}"
+VIDEO_FILE="215407_small.mp4"
+AUDIO_FILE="light-thunder.mp3"
 
 # Instalar dependencias necesarias
-apt-get update && apt-get install -y ffmpeg nginx
-
-# Configurar nginx para servir el archivo de video
-mkdir -p /var/www/html
-cp "$VIDEO_FILE" /var/www/html/video.mp4
-
-cat > /etc/nginx/sites-available/default << EOF
-server {
-    listen $SERVER_PORT default_server;
-    listen [::]:$SERVER_PORT default_server;
-    
-    root /var/www/html;
-    
-    location / {
-        add_header 'Access-Control-Allow-Origin' '*';
-        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
-        try_files \$uri \$uri/ =404;
-    }
-}
-EOF
-
-# Iniciar nginx
-service nginx start
+apt-get update && apt-get install -y ffmpeg
 
 # Iniciar la transmisión a YouTube
-#ffmpeg -stream_loop -1 \
-#    -re -i "http://localhost:$SERVER_PORT/video.mp4" \
-#    -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -g 50 \
-#    -c:a aac -b:a 160k -ar 44100 \
-#    -f flv "rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY"
-
-ffmpeg -stream_loop -1 \
-    -re -i "http://localhost:$SERVER_PORT/video.mp4" \
-    -c:v libx264 -preset ultrafast -maxrate 400k -bufsize 3200k -g 50 \
+ffmpeg -re -stream_loop -1 -i "$VIDEO_FILE" \
+    -stream_loop -1 -i "$AUDIO_FILE" \
+    -map 0:v:0 -map 1:a:0 \
+    -c:v copy \
     -c:a aac -b:a 128k -ar 44100 \
     -f flv "rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY"
-
-    
-
-
-    
